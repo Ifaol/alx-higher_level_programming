c3e11f58fd2545500f26649b1bc3918dacd2b1fb
Ifaol <ifaolhunde@gmail.com>
2023-11-27 03:45:42 -0800

    Updated for variable nameing correction

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-s = "Python is an interpreted, interactive, object-oriented programming\
+str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-s = " ".join([s.split()[5], s.split()[6], s.split()[12], s.split()[0]])
-print(s)
+str = " ".join([str.split()[5], str.split()[6], str.split()[12], str.split()[0]])
+print(str)
