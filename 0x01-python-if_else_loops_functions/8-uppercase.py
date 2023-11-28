1b19e64b926ca6a8554125119e4853c127c77c8d
Ifaol <ifaolhunde@gmail.com>
2023-11-28 04:24:09 -0800

    A python function that prints in all upper case added

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        val = ord(c)
+        if val >= 97 and val <= 122:
+            print("{}".format(chr(val -32)), end="")
+        else:
+            print("{}".format(c), end="")
+    print()
