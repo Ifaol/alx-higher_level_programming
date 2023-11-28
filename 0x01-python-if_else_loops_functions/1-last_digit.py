cce549eda48cb9dcf36b934936c4309d8ab80a82
Ifaol <ifaolhunde@gmail.com>
2023-11-28 01:47:42 -0800

    A python programme that identifies the last digit of a number and give info relative to its position to 5 added

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+num = abs(number) % 10
+str = "Last digit of {} is {} and is {}"
+if num == 0:
+    print(str.format(number, num, "0"))
+elif num < 6:
+    print(str.format(number, num, "less than 6 and not 0"))
+else:
+    print(str.format(number, num, "greater than 5"))
