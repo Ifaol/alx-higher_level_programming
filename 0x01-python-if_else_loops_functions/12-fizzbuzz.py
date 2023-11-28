57b79bf99d756d9c8444055f644b5e62872a0bfd
Ifaol <ifaolhunde@gmail.com>
2023-11-28 05:09:55 -0800

    Updated for pycodestyle compliance

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,7 +4,7 @@ def fizzbuzz():
         val1 = n % 3
         val2 = n % 5
         if val1 == 0 and val2 == 0:
-            print("{}".format("FizzBuzz"), end = " ")
+            print("{}".format("FizzBuzz"), end=" ")
         elif val1 == 0:
             print("{}".format("Fizz"), end=" ")
         elif val2 == 0:
