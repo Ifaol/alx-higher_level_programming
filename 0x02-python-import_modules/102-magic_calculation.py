b4662b4e0b0edc722be097661d354eb4ec5dccb6
Ifaol <ifaolhunde@gmail.com>
2023-11-30 05:47:59 -0800

    Updated for pycodestyle compliance

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+m = __import__('magic_calculation_102', globals(), locals(), ['add', 'sub'], 0)
+add, sub = m.add, m.sub
+
+
 def magic_calculation(a, b):
-    add, sub = __import__('magic_calculation_102', globals(), locals(), ['add', 'sub'], 0).add, __import__('magic_calculation_102', globals(), locals(), ['add', 'sub'], 0).sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
