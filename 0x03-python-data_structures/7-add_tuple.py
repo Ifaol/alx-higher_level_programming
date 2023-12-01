3c74413af896b1a81c8c1630facf7475c97de520
Ifaol <ifaolhunde@gmail.com>
2023-12-01 09:18:53 -0800

    A python function that add two tuples added

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = 0
+    b = 0
+    c = 0
+    d = 0
+    len1 = len(tuple_a)
+    len2 = len(tuple_b)
+    if len1 == 2:
+        a = int(tuple_a[0])
+        b = int(tuple_a[1])
+    elif len1 == 1:
+        a = int(tuple_a[0])
+    if len2 == 2:
+        c = int(tuple_b[0])
+        d = int(tuple_b[1])
+    elif len2 == 1:
+        c = int(tuple_b[0])
+    return (a + c, b + d)
