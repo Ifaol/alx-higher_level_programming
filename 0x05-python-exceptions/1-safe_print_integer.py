9e83caaee01cdcf227b39e4638e0b96db7512616
Ifaol <ifaolhunde@gmail.com>
2023-12-18 04:03:25 -0800

    A function that prints an integer with {:d}.format() updated

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     bol = True
     try:
-        print("{:d}".format(value))
+        print("{:d}".format(int(value)))
     except ValueError:
         bol = False
     return bol
