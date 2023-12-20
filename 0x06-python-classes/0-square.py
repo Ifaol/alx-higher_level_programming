008d37450e7460e5bea8d1bce95500f961ab2166
Ifaol <ifaolhunde@gmail.com>
2023-12-20 06:54:30 -0800

    An empty class Square that defines a square added

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,18 @@
+"""
+This module defines a Square class.
+
+The Square class represents a square shape. It is initially an empty class,
+but it can be extended with attributes and methods for specific use cases.
+"""
+
+class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        No attributes are defined for an empty square.
+
+    Methods:
+        No methods are defined for an empty square.
+    """
+    pass
