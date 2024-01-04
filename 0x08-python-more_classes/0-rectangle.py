c646bcd9727308f2f43e41d8ba76f3f7eecf4c8c
Ifaol <ifaolhunde@gmail.com>
2024-01-04 02:40:16 -0800

    test run 2

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 class Rectangle:
-    pass
+    """A class representing a rectangle.
+
+    Attributes:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+    """
+
+    def __init__(self):
+        pass
