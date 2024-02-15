b6ad2a90c2b28bd4e0d930bae6f83dd0b08516e6
Ifaol <ifaolhunde@gmail.com>
2024-02-15 11:34:26 -0800

    Test

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -10,7 +10,7 @@
 
 -- Query to select all cities of California.
 
-SELECT cities.name 
+SELECT cities.id, cities.name 
 FROM cities, states 
 WHERE cities.state_id = states.id 
 AND states.name = 'California'
